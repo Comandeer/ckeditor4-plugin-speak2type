5d0acd9f81807f525fe4c370dda46b48bb83bd3b
Comandeer <vepomoc@gmail.com>
2016-02-28 21:36:09 +0100

    Working demo.

---
+++ plugins/speak2type/lang/en.js
@@ -0,0 +1,8 @@
+/**
+ * @license Copyright (c) 2016, Comandeer. All rights reserved.
+ * For licensing, see LICENSE.
+ */
+
+CKEDITOR.plugins.setLang( 'speak2type', 'en', {
+	label: 'Speak to type'
+} );
